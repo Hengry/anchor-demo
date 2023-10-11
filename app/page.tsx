5e336f02e10ba8dfe4afd246e159e6fb172a04ee
Heng Lee <hengry1213@gmail.com>
2023-10-11 15:47:40 +0800

    basic frame for host

--- app/page.tsx
+++ app/page.tsx
@@ -3,32 +3,100 @@
 import Image from 'next/image';
 import tw from 'twin.macro';
 import { twJoin } from 'tailwind-merge';
+import { TypeAnimation } from 'react-type-animation';
+import { useState, useMemo } from 'react';
+
+interface MessageType {
+  type: 'image' | 'text';
+  content: string;
+}
+
+interface Stage {
+  from: 'host' | 'user';
+  messages: Array<MessageType>;
+}
+
+const data: Stage[] = [
+  {
+    from: 'host',
+    messages: [
+      {
+        type: 'text',
+        content: 'The king says, “that plot of land is yours to keep.”',
+      },
+      {
+        type: 'image',
+        content: 'https://i.imgur.com/FKemFke.png',
+      },
+      {
+        type: 'text',
+        content: 'what should we do next?(the 3rd response after 6:50 pm)',
+      },
+    ],
+  },
+];
+
+interface MessageProps extends MessageType {
+  onFinished: () => void;
+}
+const Message = ({ type, content, onFinished }: MessageProps) => {
+  if (type === 'image')
+    return (
+      <Image
+        // className='relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert'
+        src={content}
+        alt='Next.js Logo'
+        width={180}
+        height={180}
+        priority
+        onLoadingComplete={onFinished}
+      />
+    );
+  return (
+    <TypeAnimation
+      sequence={[1500, content, 500, onFinished]}
+      wrapper='div'
+      speed={80}
+      cursor={false}
+    />
+  );
+};
 
 const Title = tw.div`p-8`;
 export default function Home() {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const injectedData = useMemo(() => {
+    let index = 0;
+    return data.map((stage) => ({
+      ...stage,
+      messages: stage.messages.map((message) => ({
+        ...message,
+        index: index++,
+      })),
+    }));
+  }, []);
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <div
-        className={twJoin([
-          'relative flex place-items-center z-[-1]',
-          'before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full ',
-          "before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-['']",
-          'before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 before:lg:h-[360px]',
-          'after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3',
-          "after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] ",
-          'after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40',
-        ])}
-      >
-        <Image
-          className='relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert'
-          src='/next.svg'
-          alt='Next.js Logo'
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-      <Title>Starter from Hengry</Title>
+      {injectedData[0].messages.map(({ index, type, content }) =>
+        index <= activeIndex ? (
+          <Message
+            key={index}
+            type={type}
+            content={content}
+            onFinished={() => {
+              setActiveIndex((prev) => prev + 1);
+            }}
+          />
+        ) : null
+      )}
+      {/* <Image
+        className='relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert'
+        src='/next.svg'
+        alt='Next.js Logo'
+        width={180}
+        height={37}
+        priority
+      /> */}
     </main>
   );
 }
--- next.config.js
+++ next.config.js
@@ -6,6 +6,16 @@ const nextConfig = withTwin({
   experimental: {
     serverActions: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 });
 
 module.exports = nextConfig;
