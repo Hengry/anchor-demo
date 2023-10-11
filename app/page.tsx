15254489d8c86d257e525e61dff9b1bceeddd565
Heng Lee <hengry1213@gmail.com>
2023-10-11 16:19:57 +0800

    add user input

--- app/page.tsx
+++ app/page.tsx
@@ -9,14 +9,19 @@ import { useState, useMemo } from 'react';
 interface MessageType {
   type: 'image' | 'text';
   content: string;
+  selected?: boolean;
+}
+interface MessageInjected extends MessageType {
+  index: number;
+  isLastOfTheStage: boolean;
 }
 
-interface Stage {
+interface StageType {
   from: 'host' | 'user';
   messages: Array<MessageType>;
 }
 
-const data: Stage[] = [
+const data: StageType[] = [
   {
     from: 'host',
     messages: [
@@ -34,12 +39,28 @@ const data: Stage[] = [
       },
     ],
   },
+  {
+    from: 'user',
+    messages: [
+      { type: 'text', content: 'Plant cannabisEarly' },
+      { type: 'text', content: 'Plant cannabis222' },
+      { type: 'text', content: 'Plant cannabis12314' },
+      { type: 'text', content: 'Plant cannabis' },
+      { type: 'text', content: 'Plant cannabis444' },
+    ],
+  },
 ];
 
 interface MessageProps extends MessageType {
-  onFinished: () => void;
+  onFinished: (isTheLast: boolean) => void;
+  isLastOfTheStage: boolean;
 }
-const Message = ({ type, content, onFinished }: MessageProps) => {
+const Message = ({
+  type,
+  content,
+  onFinished,
+  isLastOfTheStage,
+}: MessageProps) => {
   if (type === 'image')
     return (
       <Image
@@ -49,54 +70,88 @@ const Message = ({ type, content, onFinished }: MessageProps) => {
         width={180}
         height={180}
         priority
-        onLoadingComplete={onFinished}
+        onLoadingComplete={() => {
+          onFinished(isLastOfTheStage);
+        }}
       />
     );
   return (
     <TypeAnimation
-      sequence={[1500, content, 500, onFinished]}
+      sequence={[
+        1500,
+        content,
+        500,
+        () => {
+          onFinished(isLastOfTheStage);
+        },
+      ]}
       wrapper='div'
       speed={80}
       cursor={false}
     />
   );
 };
+interface StageProps {
+  messages: Array<MessageInjected>;
+  activeIndex: number;
+  onFinished: (isTheLast: boolean) => void;
+  type: 'host' | 'user';
+}
+const Stage = ({ type, messages, activeIndex, onFinished }: StageProps) => (
+  <div className={type === 'user' ? '' : ''}>
+    {messages.map(({ index, type, content, isLastOfTheStage }) =>
+      index <= activeIndex ? (
+        <Message
+          key={index}
+          type={type}
+          content={content}
+          onFinished={onFinished}
+          isLastOfTheStage={isLastOfTheStage}
+        />
+      ) : null
+    )}
+  </div>
+);
 
-const Title = tw.div`p-8`;
+// const Title = tw.div`p-8`;
 export default function Home() {
   const [activeIndex, setActiveIndex] = useState(0);
+
   const injectedData = useMemo(() => {
     let index = 0;
-    return data.map((stage) => ({
+    return data.map((stage, stageIndex) => ({
       ...stage,
-      messages: stage.messages.map((message) => ({
+      stageIndex,
+      messages: stage.messages.map((message, i) => ({
         ...message,
+        isLastOfTheStage: i === stage.messages.length - 1,
         index: index++,
       })),
     }));
   }, []);
+
+  const [isWaitingForInput, setIsWaitingForInput] = useState(false);
+
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      {injectedData[0].messages.map(({ index, type, content }) =>
-        index <= activeIndex ? (
-          <Message
-            key={index}
-            type={type}
-            content={content}
-            onFinished={() => {
-              setActiveIndex((prev) => prev + 1);
+    <main className='w-full h-screen relative flex flex-col p-8'>
+      <div className='flex-1'>
+        {injectedData.map(({ from, messages, stageIndex }) => (
+          <Stage
+            key={stageIndex}
+            type={from}
+            messages={messages}
+            activeIndex={activeIndex}
+            onFinished={(isLast) => {
+              if (isLast) setIsWaitingForInput(true);
+              else setActiveIndex((prev) => prev + 1);
             }}
           />
-        ) : null
-      )}
-      {/* <Image
-        className='relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert'
-        src='/next.svg'
-        alt='Next.js Logo'
-        width={180}
-        height={37}
-        priority
-      /> */}
+        ))}
+      </div>
+      <div className='w-full flex'>
+        <input className='flex-1 text-black' disabled={!isWaitingForInput} />
+        <
+      </div>
     </main>
   );
 }
